eda744bef31f0bfc44a7a0ca6af4682fa67dbc81
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-07 01:04:31 +0530

    refactor: Improve readability of code using arrow functions

--- main.js
+++ main.js
@@ -14,9 +14,5 @@ const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
 
 alice1.animate(aliceTumbling, aliceTiming).finished
-  .then(() => {
-    alice2.animate(aliceTumbling, aliceTiming).finished
-      .then(() => {
-        alice3.animate(aliceTumbling, aliceTiming);
-      });
-  });
+  .then(() => alice2.animate(aliceTumbling, aliceTiming).finished)
+  .then(() => alice3.animate(aliceTumbling, aliceTiming));
